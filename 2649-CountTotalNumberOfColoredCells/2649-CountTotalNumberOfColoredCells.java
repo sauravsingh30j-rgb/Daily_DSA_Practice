250fedb8cf666bc0aa1e3b5e2b947f1a5d863713
Saurav Singh <saurav.singh.30j@gmail.com>
2025-08-27 23:21:32 +0530

    Create 2649-CountTotalNumberOfColoredCells

---
+++ 2649-CountTotalNumberOfColoredCells/2649-CountTotalNumberOfColoredCells.java
@@ -0,0 +1,8 @@
+// Last updated: 27/08/2025, 23:21:32
+class Solution {
+    public long coloredCells(int n) {
+        long ans=(long)(Math.pow(n,2))+(long)(Math.pow(n-1,2));
+        return ans;
+        
+    }
+}
