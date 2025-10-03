4c2a1da6b1d8975512b86b9e7e2e24fbd358ed86
Saurav Singh <saurav.singh.30j@gmail.com>
2025-10-04 01:43:37 +0530

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.java
@@ -0,0 +1,20 @@
+// Last updated: 04/10/2025, 01:43:37
+
+public class Solution extends VersionControl {
+	public int firstBadVersion(int n) {
+		int lo = 1;
+		int hi = n;
+		int ans = 0;
+		while (lo <= hi) {
+			int mid = lo+( hi-lo) / 2;
+			if (isBadVersion(mid) == true) {
+				ans = mid;
+				hi = mid - 1;
+			} else {
+				lo = mid + 1;
+			}
+		}
+		return ans;
+
+    }
+}
