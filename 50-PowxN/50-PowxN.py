63c06589f8785c34c69ac3a19bad140c505b8737
Saurav Singh <saurav.singh.30j@gmail.com>
2025-10-04 01:46:03 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.py
@@ -0,0 +1,12 @@
+# Last updated: 04/10/2025, 01:46:02
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        import math
+        k=pow(x,n)
+        
+        return ((k))
