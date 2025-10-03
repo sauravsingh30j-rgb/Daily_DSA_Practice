c0630643a406afaa3d5df86ad102f980868ef7c6
Saurav Singh <saurav.singh.30j@gmail.com>
2025-10-04 01:46:18 +0530

    Create 32-LongestValidParentheses

---
+++ 32-LongestValidParentheses/32-LongestValidParentheses.java
@@ -0,0 +1,20 @@
+// Last updated: 04/10/2025, 01:46:17
+public class Solution {
+    public int longestValidParentheses(String s) {
+        int maxLen = 0;
+        int[] dp = new int[s.length()];
+        
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxLen = Math.max(maxLen, dp[i]);
+            }
+        }
+        
+        return maxLen;
+    }
+}
