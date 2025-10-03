753abfb2a156ba298085989c37a02f05106dde61
Saurav Singh <saurav.singh.30j@gmail.com>
2025-10-04 01:46:06 +0530

    Create 43-MultiplyStrings

---
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -0,0 +1,10 @@
+# Last updated: 04/10/2025, 01:46:06
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        k=(int(num1)*int(num2))
+        return str(k)
