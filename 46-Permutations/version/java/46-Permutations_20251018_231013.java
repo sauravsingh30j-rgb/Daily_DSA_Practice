56d39d7c2c06a83b5dcc42eb64fdeb6b8c3b8ac5
Saurav Singh <saurav.singh.30j@gmail.com>
2025-10-18 23:10:14 +0530

    Create 46-Permutations

---
+++ 46-Permutations/version/java/46-Permutations_20251018_231013.java
@@ -0,0 +1,21 @@
+// Last updated: 18/10/2025, 23:10:13
+class Solution {
+    void nextPermutation(int arr[],List<Integer>temp,List<List<Integer>> nums,int start){
+        if(temp.size()==arr.length){
+            nums.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        for(int i=0;i<arr.length;i++){
+            if(temp.contains(arr[i]))continue;
+            temp.add(arr[i]);
+            nextPermutation(arr,temp,nums,i+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        nextPermutation(nums,new ArrayList<>(),res,0);
+        return res;
+    }
+}
