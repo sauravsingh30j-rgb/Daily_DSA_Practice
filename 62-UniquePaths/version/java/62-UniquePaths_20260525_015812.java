ec54ee56a4e8cd847ea88d221a11f368856b30d0
Saurav Singh <saurav.singh.30j@gmail.com>
2026-05-25 01:58:14 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/version/java/62-UniquePaths_20260525_015812.java
@@ -0,0 +1,18 @@
+// Last updated: 25/05/2026, 01:58:12
+1class Solution {
+2    public int uniquePaths(int m, int n) {
+3        int[] aboveRow = new int[n];
+4        Arrays.fill(aboveRow, 1);
+5
+6        for (int row = 1; row < m; row++) {
+7            int[] currentRow = new int[n];
+8            Arrays.fill(currentRow, 1);
+9            for (int col = 1; col < n; col++) {
+10                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+11            }
+12            aboveRow = currentRow;
+13        }
+14
+15        return aboveRow[n - 1];        
+16    }
+17}
