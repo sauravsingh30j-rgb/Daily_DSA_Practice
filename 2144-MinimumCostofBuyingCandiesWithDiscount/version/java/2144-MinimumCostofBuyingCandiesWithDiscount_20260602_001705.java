a587deff420dbfde1566f7c601986de0863bb227
Saurav Singh <saurav.singh.30j@gmail.com>
2026-06-02 00:17:07 +0530

    Create 2144-MinimumCostofBuyingCandiesWithDiscount

---
+++ 2144-MinimumCostofBuyingCandiesWithDiscount/version/java/2144-MinimumCostofBuyingCandiesWithDiscount_20260602_001705.java
@@ -0,0 +1,15 @@
+// Last updated: 02/06/2026, 00:17:05
+1class Solution {
+2    public int minimumCost(int[] cost) {
+3        Arrays.sort(cost);
+4        int total = 0;
+5
+6        for (int i = cost.length - 1; i >= 0; i -= 3) {
+7            total += cost[i];
+8            if (i - 1 >= 0)
+9                total += cost[i - 1];
+10        }
+11
+12        return total;
+13    }
+14}
